c67ca5b685c479145072728fccb4504fbad5949f
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-07-10 09:12:18 +0200

    CS fixes

--- LightSmsTransport.php
+++ LightSmsTransport.php
@@ -86,7 +86,7 @@ final class LightSmsTransport extends AbstractTransport
 
     public function __toString(): string
     {
-        return sprintf('lightsms://%s?from=%s', $this->getEndpoint(), $this->from);
+        return \sprintf('lightsms://%s?from=%s', $this->getEndpoint(), $this->from);
     }
 
     public function supports(MessageInterface $message): bool
@@ -109,7 +109,7 @@ final class LightSmsTransport extends AbstractTransport
         ];
         $data['signature'] = $this->generateSignature($data);
 
-        $endpoint = sprintf('https://%s/external/get/send.php', $this->getEndpoint());
+        $endpoint = \sprintf('https://%s/external/get/send.php', $this->getEndpoint());
         $response = $this->client->request('GET', $endpoint, [
             'query' => $data,
         ]);
